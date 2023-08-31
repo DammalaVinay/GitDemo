d69ab0e0493c921e2fb18365d48556e5506e4bc0
DammalaVinay <dammala.vinay1008@gmail.com>
2023-08-31 11:13:04 -0500

    Other Guy

--- src/main/java/gitDemo/CommandsThree.java
+++ src/main/java/gitDemo/CommandsThree.java
@@ -1,5 +1,15 @@
 package gitDemo;
 
+import org.testng.annotations.Test;
+
 public class CommandsThree {
+	
+	@Test
+	public void sampleTestThree() {
+		
+		System.out.println("API test3");
+		
+	}
+	
 
 }
