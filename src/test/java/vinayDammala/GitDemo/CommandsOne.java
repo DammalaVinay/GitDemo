34b7c0fff1e7bd3535ae28f38fd7ccfbd6456122
DammalaVinay <dammala.vinay1008@gmail.com>
2023-08-31 10:10:21 -0500

    Initial Commit

---
+++ src/test/java/vinayDammala/GitDemo/CommandsOne.java
@@ -0,0 +1,15 @@
+package vinayDammala.GitDemo;
+
+import org.testng.annotations.Test;
+
+public class CommandsOne {
+
+	@Test
+	public void sampleTest() {
+		
+		System.out.println("Git Commit Test1");
+		
+		
+	}
+	
+}
---
+++ src/test/java/vinayDammala/GitDemo/CommandsTwo.java
@@ -0,0 +1,15 @@
+package vinayDammala.GitDemo;
+
+import org.testng.annotations.Test;
+
+public class CommandsTwo {
+	
+	@Test
+	public void sampleTest2() {
+		
+		System.out.println("Git Commit Test 2");
+		
+		
+	}
+
+}
